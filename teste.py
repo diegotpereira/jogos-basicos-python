1918642aca79d2eafabb31c30ed6a070cc95d903
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-03-31 11:36:21 -0300

    alteração no arquivo python

---
+++ teste.py
@@ -0,0 +1,79 @@
+# print((6 + 3) - ( 6 + 3))
+# print(100 + 5 * 3)
+
+thisLista = ['maça', 'banana', 'cereja']
+# print(thisLista)
+
+# print(len(thisLista))
+
+# print(type(thisLista))
+
+# if 'maça' in thisLista:
+#     print("Sim, 'maça' está na lista de frutas")
+
+# thisLista[1] = 'uva'
+
+# print(thisLista)
+
+# thisLista.append('laranja')
+
+# print(thisLista)
+
+# thisLista.insert(1, 'Mamão')
+
+# print(thisLista)
+
+# thisLista.remove('maça')
+
+# print(thisLista)
+
+# thisLista.pop(1)
+
+# print(thisLista)
+
+# del thisLista[1]
+
+# print(thisLista)
+
+# for x in thisLista:
+#     print(x)
+
+# for i in range(len(thisLista)):
+#     print(thisLista[i])
+
+
+# i = 0
+
+# while i < len(thisLista):
+#     print(thisLista[1])
+
+#     i = i + 1
+
+# novaLista = []
+
+# for x in thisLista:
+
+#     if "e" in x: 
+
+#         novaLista.append(x)
+
+# print(novaLista)
+
+minhaTupla = ("maça", "banana", "cereja")
+
+# print(len(minhaTupla))
+# print(type(minhaTupla))
+# print(minhaTupla[1])
+
+# y = list(minhaTupla)
+# y.append("laranja")
+
+# minhaTupla = tuple(y)
+
+# print(minhaTupla)
+
+
+y = ("laranja", )
+minhaTupla += y
+
+print(minhaTupla)
